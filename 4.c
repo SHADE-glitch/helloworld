337c13f9f0b5c0fbc04d657a55c61042d0a64048
SHADE-glitch <gp160515131404qq.com@outlook.com>
2024-11-28 16:27:57 +0800

    commit 4.c

---
+++ 4.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+ int main()
+ {
+ 
+ 
+ 	int price = 0 ;
+ 	int bill = 0 ;
+ 	
+ 	printf("请输入金额:" );
+    scanf("%d", &price) ;
+		
+		printf("请输入票面： ")	;
+	scanf("%d", &bill);
+	
+	if (bill>=price)
+	{
+		printf("应该找你：%d",bill - price);
+	} 
+ 	
+ 	
+ 	return 0;
+}
+ 
